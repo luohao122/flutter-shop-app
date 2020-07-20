3d4ab348e19ca8208d7ab0861a69f44932e31c66
Jake Luong <luong_quoc_hao@sezax.co.jp>
2020-07-20 17:46:10 +0700

    first setup
    add provider package
    
    shop add orders done
    
    state done
    
    form section done
    
    use request, add product
    
    update authentication
    
    shop authentication done
    
    shop app done

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
+  @override
+  Widget build(BuildContext context) {
+    // final orderData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: const CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              // Do error handling
+              return const Center(
+                child: const Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, index) =>
+                      OrderItem(orderData.orders[index]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
